b27f7cabc07b24a47f2f3e9456e5788dfc70be58
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-05-10 00:14:11 +0600

    Cost added functionality implemented

--- src/pages/AddCost/AddCost.jsx
+++ src/pages/AddCost/AddCost.jsx
@@ -1,6 +1,41 @@
+import swal from "sweetalert";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
 
 const AddCost = () => {
+    const axiosPublic = useAxiosPublic();
 
+    const handleDonation = e => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const village = form.village.value;
+        const divission = form.divission.value;
+        const items = form.items.value;
+        const date = form.date.value;
+        const money = form.money.value;
+        // console.log(name, village, divission, items, date, money);
+
+        const donation = {
+            name,
+            village,
+            divission,
+            items,
+            date,
+            money
+        }
+        axiosPublic.post("/add/cost", donation)
+            .then(res => {
+                if (res.status === 200) {
+
+                    swal("Successful !", "Donation added", "success");
+                    form.reset();
+                }
+            })
+            .catch(error => {
+                swal("Oops", { error }, "error");
+            })
+
+    }
 
     // prevent changing amount on mouse wheel
     const handleWheel = (e) => {
@@ -11,7 +46,7 @@ const AddCost = () => {
             <div className='w-11/12 lg:w-1/2 mx-auto'>
                 <h1 className="text-5xl font-bold mb-4 text-center">Add Total Donation</h1>
                 <div className="border-2  border-blue-600 rounded-lg w-full shadow-2xl bg-base-100 px-4 py-6">
-                    <form onSubmit='' className="">
+                    <form onSubmit={handleDonation} className="">
                         <div>
                             <label className="label">
                                 <span className="text-xl font-medium">Benifited People Name</span>
@@ -34,7 +69,13 @@ const AddCost = () => {
                             <label className="label">
                                 <span className="text-xl font-medium">Given Item</span>
                             </label>
-                            <textarea className="textarea textarea-bordered h-24 rounded-md w-full border-blue-600" placeholder="List of Item" required></textarea>
+                            <textarea name="items" className="textarea textarea-bordered h-24 rounded-md w-full border-blue-600" placeholder="List of Item" required></textarea>
+                        </div>
+                        <div>
+                            <label className="label">
+                                <span className="text-xl font-medium">Date</span>
+                            </label>
+                            <input type="date" name='date' className="input rounded-md w-full border-blue-600" required />
                         </div>
                         <div>
                             <label className="label">
