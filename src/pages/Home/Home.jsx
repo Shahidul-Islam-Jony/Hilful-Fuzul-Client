b657943024756f208c7733bbdd9efd205058be1f
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-05-10 00:46:43 +0600

    DonationQuotes page added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../../components/Home/Banner/Banner";
+import DonationQuotes from "../../components/Home/DonationQuotes/DonationQuotes";
 
 const Home = () => {
     return (
         <div className="mt-20">
             <Banner></Banner>
+            <DonationQuotes></DonationQuotes>
         </div>
     );
 };
