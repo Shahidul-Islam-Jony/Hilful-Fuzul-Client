0199d2948df16cdd838941adb896c1ed9e1a3838
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-05-10 00:50:23 +0600

    Donation Quotes page responsive done

--- src/components/Home/DonationQuotes/DonationQuotes.jsx
+++ src/components/Home/DonationQuotes/DonationQuotes.jsx
@@ -6,20 +6,20 @@ import islamicFoodReserve from '../../../assets/DonationQuotes/islamic food serv
 const DonationQuotes = () => {
     return (
         <div className='w-11/12 my-10 mx-auto'>
-            <div className='flex justify-between items-center'>
+            <div className='flex flex-col lg:flex-row gap-8 justify-between items-center'>
                 <div>
                     <img className='rounded-lg' src={islamicFoodReserve} alt="" />
                 </div>
                 <div>
-                    <p className='text-xl font-medium w-[700px] text-center border-2 border-blue-600 rounded-lg p-4'>যখন কোনো ব্যক্তি মৃতু্যবরণ করে, তার সব আমল বন্ধ হয়ে যায়, তিনটি ব্যতীত, সদকায়ে জারিয়া, উপকারী জ্ঞান অথবা সত্কর্মশীল সন্তান যে তার জন্য দোয়া করে। ' (সহিহ মুসলিম শরিফ, হাদিস নম্বর-১৬৩১)</p>
+                    <p className='text-xl mb-8 font-medium md:w-[700px] text-center border-2 border-blue-600 rounded-lg p-4'>যখন কোনো ব্যক্তি মৃতু্যবরণ করে, তার সব আমল বন্ধ হয়ে যায়, তিনটি ব্যতীত, সদকায়ে জারিয়া, উপকারী জ্ঞান অথবা সত্কর্মশীল সন্তান যে তার জন্য দোয়া করে। ' (সহিহ মুসলিম শরিফ, হাদিস নম্বর-১৬৩১)</p>
                 </div>
             </div>
-            <div className='flex flex-row-reverse justify-between items-center'>
+            <div className='flex flex-col lg:flex-row-reverse gap-8 justify-between items-center'>
                 <div>
                     <img className='rounded-lg' src={poorPeopleFood} alt="" />
                 </div>
                 <div>
-                    <p className='text-xl font-medium w-[700px] text-center border-2 border-blue-600 rounded-lg p-4'> যারা আল্লাহর পথে তাদের সম্পদ ব্যয় করে, তাদের উপমা একটি বীজের মত, যা উৎপন্ন করল সাতটি শীষ, প্রতিটি শীষে রয়েছে একশ’ দানা। আর আল্লাহ যাকে চান তার জন্য বাড়িয়ে দেন। আর আল্লাহ প্রাচুর্যময়, সর্বজ্ঞ।
+                    <p className='text-xl font-medium md:w-[700px] text-center border-2 border-blue-600 rounded-lg p-4'> যারা আল্লাহর পথে তাদের সম্পদ ব্যয় করে, তাদের উপমা একটি বীজের মত, যা উৎপন্ন করল সাতটি শীষ, প্রতিটি শীষে রয়েছে একশ’ দানা। আর আল্লাহ যাকে চান তার জন্য বাড়িয়ে দেন। আর আল্লাহ প্রাচুর্যময়, সর্বজ্ঞ।
 
                         সূরাঃ আল-বাকারা আয়াতঃ ২৬১</p>
                 </div>
