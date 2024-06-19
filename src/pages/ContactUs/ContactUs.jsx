a3c9d073cf9e874fbd3e9804d8cf2497905f7c9d
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-06-19 18:27:35 +0600

    Contact us page added

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,8 +1,20 @@
 
+import memberBack from "../../assets/images/hilful-fuzul-member-2.jpg"
+import memberFront from "../../assets/images/hilful-fuzul member-1.jpg"
+
 const ContactUs = () => {
     return (
-        <div className="mt-28">
-            This is contact us page. This page is under contractions
+        <div className="mt-24 w-[97%] mx-auto">
+            <div>
+                <p className="text-2xl font-bold text-center my-4 text-blue-600 underline italic">Some of Our members</p>
+                <img src={memberFront} className="md:h-[500px] md:w-full rounded-lg" alt="" />
+                <img src={memberBack} className="md:h-[500px] md:w-full rounded-lg mt-4" alt="" />
+            </div>
+            <div>
+                <div className="text-xl font-bold mt-4">
+                    Contact number : 01734845697
+                </div>
+            </div>
         </div>
     );
 };
